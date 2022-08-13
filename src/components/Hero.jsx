a312b735413f534a75721ec2156bee5b0fdab446
seunope <seunope2004@yahoo.co.uk>
2022-08-13 23:52:00 +0100

    about us

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import {
 } from '@heroicons/react/solid';
 import bgImg from '../assets/cyber-bg.png';
 
-export const Hero = () => {
+const Hero = () => {
   return (
     <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between ">
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto ">
@@ -47,3 +47,4 @@ export const Hero = () => {
     </div>
   );
 };
+export default Hero;
